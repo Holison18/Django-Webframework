5b0c939a5fb4410a08f3ef40909d9ed927330f9b
Holison <kobbyholison2002@gmail.com>
2023-09-15 07:32:02 +0000

    email validation using regex

--- Python_fundamentals/regex.py
+++ Python_fundamentals/regex.py
@@ -1,2 +1,13 @@
 # python regular expression
 # this program verifies an email to make sure it meets the standard of an email
+import re 
+
+pattern = "[a-zA-Z0-9]+@[a-zA-z]+\.(com|edu|net)" 
+
+# ask user for input
+userInput = input()
+
+if re.search(pattern,userInput):
+    print("Valid email")
+else:
+    print("Invalid email")
