9f0fb9d33bd6191478da1fb81d14f7aef034cf91
Holison <kobbyholison2002@gmail.com>
2023-09-15 07:51:56 +0000

    contact validation with regex

--- Python_fundamentals/regex.py
+++ Python_fundamentals/regex.py
@@ -1,13 +1,25 @@
 # python regular expression
-# this program verifies an email to make sure it meets the standard of an email
+
 import re 
 
+# verify an email to make sure it meets the standard of an email
 pattern = "[a-zA-Z0-9]+@[a-zA-z]+\.(com|edu|net)" 
 
 # ask user for input
-userInput = input()
+userInput = input("Email: ")
 
 if re.search(pattern,userInput):
     print("Valid email")
 else:
     print("Invalid email")
+
+
+# check if the contact a user enters in 10 digits
+contact_pattern = r'\+233\d{9}$'
+
+# get users contact
+contact = input("Contact: ")
+if re.match(contact_pattern,contact):
+    print("Valid contact")
+else:
+    print("Invalid contact")
