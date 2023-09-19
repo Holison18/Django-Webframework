11d391cbd0dd8085f49149dc97c10ea11e4e80bb
Holison <kobbyholison2002@gmail.com>
2023-09-19 04:43:22 +0000

    even numbers using list comprehendsion

--- Python_fundamentals/listcomprehendsion.py
+++ Python_fundamentals/listcomprehendsion.py
@@ -12,3 +12,13 @@ for i in myList: # This is the traditional way of looping through a list using a
 newList = [(x+1) for x in myList]
 # print newlist
 print(newList)
+
+# Example 2, using list comprehendsion to find the square of element is a list
+myList2 = [3,2,4,5,1]
+
+squared = [x**2 for x in myList2]
+print(squared)
+
+# creating a list of even numbers
+even = [even_number for even_number in range(11) if even_number%2==0]
+print(even)
