16a581214ea3d42922d6d4b9c2d833b619e791a3
Holison <kobbyholison2002@gmail.com>
2023-09-19 06:02:22 +0000

    conditional checking using lambda

--- Python_fundamentals/lambda.py
+++ Python_fundamentals/lambda.py
@@ -14,3 +14,9 @@ print(m(2,4))
 format_numeric = lambda num: f"{num:e}" if isinstance(num,int) else f"{num:,.2f}"
 print("Int formatting: ",format_numeric(100000))
 print("Float formatting: ",format_numeric(999999.745334324))
+
+# check if the string a user enters matches the require string
+require = "name@123"
+check = lambda response: "Correct Input" if response == require else "Wrong Input"
+print(check("name@123"))
+print(check("name@1"))
