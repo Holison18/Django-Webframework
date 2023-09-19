06c95b1691fc79cd3325a5f794a8996bef855aed
Holison <kobbyholison2002@gmail.com>
2023-09-19 04:32:51 +0000

    List comprehendsion

--- Python_fundamentals/listcomprehendsion.py
+++ Python_fundamentals/listcomprehendsion.py
@@ -2,5 +2,13 @@
 # normal list
 myList = [1,2,3,4,5]
 
-for i in myList:
+for i in myList: # This is the traditional way of looping through a list using a for loop
     print(i)
+
+# python list comprehension syntax:
+# newList = [expression(element) for element in oldlist if condition]
+
+# for example incrementing every element in our list by 1
+newList = [(x+1) for x in myList]
+# print newlist
+print(newList)
