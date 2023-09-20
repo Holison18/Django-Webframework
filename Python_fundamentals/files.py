b1b651d5e2f14e64a2e5ee58a1cce41907e72136
Holison <kobbyholison2002@gmail.com>
2023-09-20 05:30:54 +0000

    file handling in python

--- Python_fundamentals/files.py
+++ Python_fundamentals/files.py
@@ -1,7 +1,7 @@
 # Python file handling
 
 # writing to a file
-with open("kobina.txt","a") as myfile:
+with open("kobina.txt","w") as myfile:
     myfile.write("My name is kobina Akofi-holison")
 myfile.close
 
@@ -10,3 +10,10 @@ with open("kobina.txt","r") as myfile:
    content =  myfile.readlines()
    print(content)
 myfile.close
+
+# handling exception
+try:
+    with open("text.txt","r") as mytxt:
+        content = mytxt.read()
+except FileExistsError:
+    print("file does not exist")
