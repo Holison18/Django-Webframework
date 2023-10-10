72eb05a0a3ced91017d8e3c3060959b07d0feae1
Holison <kobbyholison2002@gmail.com>
2023-10-10 03:53:08 +0000

    added code to html file

--- pagesApp/pages/urls.py
+++ pagesApp/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, About
+
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("", About.as_view(), name="about"),
 ]
