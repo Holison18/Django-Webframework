2438f9f680511751b386d7d7d23c126c77f2702e
Holison <kobbyholison2002@gmail.com>
2023-10-10 04:01:59 +0000

    run abouts page

--- pagesApp/pages/urls.py
+++ pagesApp/pages/urls.py
@@ -3,6 +3,6 @@ from .views import HomePageView, About
 
 
 urlpatterns = [
+    path("about/", About.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
-    path("", About.as_view(), name="about"),
 ]
