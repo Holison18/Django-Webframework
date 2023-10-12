ec294d0192795124b574f9569540bb1ad2f87021
Holison <kobbyholison2002@gmail.com>
2023-10-12 06:40:00 +0000

    added contact us to urls.py

--- pagesApp/pages/urls.py
+++ pagesApp/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, About
+from .views import HomePageView, About, contactUs
 
 
 urlpatterns = [
     path("about/", About.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
+    path("ContactUs/", contactUs.as_view(), name="contactUs"),
 ]
