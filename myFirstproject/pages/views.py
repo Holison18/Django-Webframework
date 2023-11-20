f8cbb47ed3ec5c47e935f46bdf7e2705e7b98411
Holison <kobbyholison2002@gmail.com>
2023-11-20 12:34:06 +0000

    urls.py

--- myFirstproject/pages/views.py
+++ myFirstproject/pages/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_page_view(request):
-    return HttpResponse("Welcome to the Django web framework")
+    return HttpResponse("""Hello there👋! My name is Kobina Akofi-Holison. I am a computer engineering student""")
