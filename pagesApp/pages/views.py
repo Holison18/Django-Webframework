7805e84847d847087cb4904f2779fdd30d72db16
Holison <kobbyholison2002@gmail.com>
2023-11-21 03:28:14 +0000

    created a template view for the abouts page

--- pagesApp/pages/views.py
+++ pagesApp/pages/views.py
@@ -1,6 +1,15 @@
 from django.views.generic.base import TemplateView
 
-# create a template view for the homepage
 
+# create a template view for the homepage
 class homePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
+
+# create a template view for the about page
+class aboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+# create a template view for the contact page
+class contactPageView(TemplateView):
+    template_name = 'contact.html'
