ae4d58d7436f5fb7e94af8c02a7da023c48ffd58
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-22 12:11:19 +0000

    run test

--- pagesApp/pages/tests.py
+++ pagesApp/pages/tests.py
@@ -25,23 +25,23 @@ class homePageTests(SimpleTestCase):
         self.assertContains(response, "<h1>Home Page</h1>")
 
     # about page test
-    class aboutPageTests(SimpleTestCase):
-        # url test
-        def urls_successful_test(self):
-            response = self.client.get("/about/")
-            self.assertEqual(response.status_code, 200)
+class aboutPageTests(SimpleTestCase):
+    # url test
+    def urls_successful_test(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
 
-        # url name available test
-        def test_url_name_available(self):
-            response = self.client.get(reverse("about"))
-            self.assertEqual(response.status_code, 200)
+    # url name available test
+    def test_url_name_available(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
 
-        # check html template
-        def test_template_name_available(self):
-            response = self.client.get(reverse("about"))
-            self.assertTemplateUsed(response, "about.html")
+    # check html template
+    def test_template_name_available(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
 
-        # check template content
-        def test_template_content(self):
-            response = self.client.get("about")
-            self.assertContains(response, "<h1>About Page</h1>")
+    # check template content
+    def test_template_content(self):
+        response = self.client.get("about")
+        self.assertContains(response, "<h1>About Page</h1>")
