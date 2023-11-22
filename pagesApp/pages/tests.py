445c0d2ffbe8e2fd57f3bfe9ea7cec3ab63eb6a7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-22 03:31:22 +0000

    test url name

--- pagesApp/pages/tests.py
+++ pagesApp/pages/tests.py
@@ -1,13 +1,24 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
 
 
 # create a test for the homepage
 class homePageTests(SimpleTestCase):
-    pass
+    # test if url returns successfully
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    # test to see if url name returns successfully
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
 
 
 # create a test for the about page
 class aboutPageTests(SimpleTestCase):
-    pass
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
