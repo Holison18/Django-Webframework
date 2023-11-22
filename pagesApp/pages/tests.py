5bee3f41f7508989b1f9e56e27570c63c521ff74
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-22 03:20:58 +0000

    create SimpleTestCases for home and about

--- pagesApp/pages/tests.py
+++ pagesApp/pages/tests.py
@@ -2,4 +2,12 @@ from django.test import SimpleTestCase
 
 # Create your tests here.
 
+
 # create a test for the homepage
+class homePageTests(SimpleTestCase):
+    pass
+
+
+# create a test for the about page
+class aboutPageTests(SimpleTestCase):
+    pass
