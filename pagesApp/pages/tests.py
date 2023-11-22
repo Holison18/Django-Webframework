68ae27b65a9cad5ffb4144ddde8ca77ae9e2c31c
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-22 03:31:49 +0000

    test url name

--- pagesApp/pages/tests.py
+++ pagesApp/pages/tests.py
@@ -14,11 +14,16 @@ class homePageTests(SimpleTestCase):
     # test to see if url name returns successfully
     def test_url_available_by_name(self):
         response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
 
 # create a test for the about page
 class aboutPageTests(SimpleTestCase):
     def test_url_exist_at_correct_location(self):
         response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+        # test to see if url name returns successfully
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
