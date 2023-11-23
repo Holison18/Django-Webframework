eff7262536e65e8796b9f0ac64842f608de64601
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-23 04:30:17 +0000

    settings.py

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "posts.PostsConfig",
+    "posts.apps.PostsConfig",
 ]
 
 MIDDLEWARE = [
