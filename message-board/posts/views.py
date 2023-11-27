270a35de96d8f734dd7ec0d3dcc191f2d0b9e05d
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-27 03:34:20 +0000

    created a new ListView

--- message-board/posts/views.py
+++ message-board/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+# create a Listview for our homepage
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
