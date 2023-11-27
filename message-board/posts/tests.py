7b5500547a70ca43f06a2f88f27abeb6547db1ed
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-27 04:48:25 +0000

    running tests

--- message-board/posts/tests.py
+++ message-board/posts/tests.py
@@ -1,29 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
-
-# import model
 from .models import Post
 
+# create a test for the posts app
+class PostTtests(TestCase):
 
-# Create your tests here.
-class PostTests(TestCase):
+    # create a temporary data base using setUpTestData function
     @classmethod
     def setUpTestData(cls) -> None:
-        cls.post = Post.objects.create(text="This is just a text")
+        cls.post = Post.objects.create(text = "Testing Text")
 
-    # tesf for model content
-    def test_model_content(self):
-        self.assertEqual(self.post.text, "This is just a text")
+    # test if database works correctly
+    def test_model_available(self):
+        self.assertEqual(self.post.text, "Testing Text")
 
-    # test if url exist at location
-    def test_url_correct_location(self):
+    # check if url exist at the correct location
+    def test_url_exist_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    # home page unit tests
+    # create a unit text for homepage
     def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "<h1>Message Board Home Page</h1>")
-        
