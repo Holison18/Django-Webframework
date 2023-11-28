f6ade3513d77fa596bdbc114c95aed73a1ca0f14
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-11-28 03:52:32 +0000

    creat database model

--- Blog App/blog/models.py
+++ Blog App/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+# create a database model for our blog app
+class Post(models.Model):
+    #fields
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={"pk":self.pk})
