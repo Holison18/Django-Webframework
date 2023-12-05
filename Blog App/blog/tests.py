66ae0421fa86d906ed189192fc5bcd11173dd545
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-05 03:41:10 +0000

    tests

--- Blog App/blog/tests.py
+++ Blog App/blog/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
@@ -8,7 +9,7 @@ class BlogTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(
-            name="testuser", email="test@email.com", password="secrete"
+            username="testuser", email="test@email.com", password="secrete"
         )
 
         cls.post = Post.objects.create(title="Title", body="Body", author=cls.user)
@@ -19,3 +20,11 @@ class BlogTest(TestCase):
         self.assertEqual(self.post.author.username,"testuser")
         self.assertEqual(str(self.post),"Title")
         self.assertEqual(self.post.get_absolute_url(),"/post/1/")
+
+    def test_if_url_location_exists(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
+
+    def test_blog_app(self):
+        
+
