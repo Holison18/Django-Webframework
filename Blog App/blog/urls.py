5bbf28d0ba1c06a3a7f5dace3c3c319d36640047
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-07 03:42:40 +0000

    CreateView

--- Blog App/blog/urls.py
+++ Blog App/blog/urls.py
@@ -3,6 +3,6 @@ from .views import BlogListView,BlogDetailView,CreateBlogView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"), #home
-    path("post/<int:pk>/",BlogDetailView.as_view(),name="post_detail")
-    path("post_new",CreateBlogView.as_view(),name="post_new")
+    path("post/<int:pk>/",BlogDetailView.as_view(),name="post_detail"),
+    path("post/new/",CreateBlogView.as_view(),name="post_new"),
 ]
--- Blog App/blog/views.py
+++ Blog App/blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # create a view for the home page
@@ -15,3 +16,4 @@ class BlogDetailView(DetailView):
 class CreateBlogView(CreateView):
     model = Post
     template_name = "post_new.html"
+    fields = ["title","author","body"]
