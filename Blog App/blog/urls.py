060f4ef6e125397f2c0bf5b6077578648215c7b7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-07 04:17:45 +0000

    add url path to UpdateBlogView

--- Blog App/blog/urls.py
+++ Blog App/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogListView,BlogDetailView,CreateBlogView
+from .views import BlogListView,BlogDetailView,CreateBlogView,UpdateBlogView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"), #home
     path("post/<int:pk>/",BlogDetailView.as_view(),name="post_detail"),
     path("post/new/",CreateBlogView.as_view(),name="post_new"),
+    path("post/new/",UpdateBlogView.as_view(),name="post_update"),
 ]
