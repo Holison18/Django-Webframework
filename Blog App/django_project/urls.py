f460d4089a07fd097359e01aadeca65a5d3bfea2
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-12 08:25:19 +0000

    runserver

--- Blog App/django_project/urls.py
+++ Blog App/django_project/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/",include("django.contrib.auth.urls")), # new
-    path("accounts/",include("accounts.url")), # new
+    path("accounts/",include("accounts.urls")), # new
     path("",include("blog.urls")), # new
 ]
