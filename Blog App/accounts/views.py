bed881a36e5a32e8932e36314038a6daa4d15de7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-12 08:17:11 +0000

    create a new template for signup

--- Blog App/accounts/views.py
+++ Blog App/accounts/views.py
@@ -3,3 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 # create a signup view
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
