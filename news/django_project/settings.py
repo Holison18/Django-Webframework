1a5cd36d201d2d4e7ab12674c9f270c807ddc273
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-13 06:32:31 +0000

    AUTH_USER_MODEL

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -110,3 +110,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# add custom user model
+AUTH_USER_MODEL = "accounts.CustomUser"
