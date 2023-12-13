b203de84f7411aebeb56d98d84356ea29027c37a
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-13 06:34:32 +0000

    models.py

--- news/accounts/models.py
+++ news/accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+# create custome user model
+class CustomUser(AbstractUser):
+    pass
