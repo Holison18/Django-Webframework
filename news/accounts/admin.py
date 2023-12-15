95b423e6d994c3ca8163a00bd376fa99fdb8f233
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-15 07:42:48 +0000

    admin.py

--- news/accounts/admin.py
+++ news/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+
+# create a custom user admin class
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+    list_display = ["email", "username", "age", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
