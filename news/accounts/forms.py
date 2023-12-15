427f6e4131a514599688dba5e3fa1c8e1f8625a1
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-15 07:18:28 +0000

    Created a usercreation form

--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -1,3 +1,6 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
+class CustomCreationForm(UserCreationForm):
+    model = CustomUser
+    fields = UserCreationForm.Meta.fields + ("age",)
