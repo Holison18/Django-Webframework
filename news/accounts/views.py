3948e40092efd363cbd62a6068f9da6fbf8444b7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-18 07:56:58 +0000

    signup view

--- news/accounts/views.py
+++ news/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+# create a view for signup
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = 'signup.html'
