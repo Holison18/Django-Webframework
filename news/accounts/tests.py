1e0943158b9d5ce3b7644d1977fa2c3752a38ab7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-19 04:26:19 +0000

    test.py

--- news/accounts/tests.py
+++ news/accounts/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+class SignupPageTests(TestCase):
+    # test if url exist at the correct location
+    def test_url_exits_at_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code,200)
+
+    # test if view name exist
+    def test_view_name_exits(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            
+        )
