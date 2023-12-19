3de87d7bc00a760a3806a13278b6b0f6eedd4d51
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-19 13:22:19 +0000

    tests.py

--- news/pages/tests.py
+++ news/pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+# create a simple test for the home page
+class HomePageTests(SimpleTestCase):
+
+    # check if url exists at the correct location
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
+
+    def test_template_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'home.html')
+        self.assertContains(response,"Home")
