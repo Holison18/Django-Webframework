330a9b0c5324a7665def605ba6f1e3e0ecf0e249
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-19 03:48:18 +0000

    form.py

--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -2,10 +2,12 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    model = CustomUser
-    fields = UserCreationForm.Meta.fields + ("age",)
+    class Meta(UserCreationForm): 
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age",)
 
 # create a class for UserChangeForm
 class CustomUserChangeForm(UserChangeForm):
-    model = CustomUser
-    fields = UserChangeForm.Meta.fields
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
