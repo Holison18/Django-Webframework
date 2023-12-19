1d649debee5d469c763f083ca2463c2e5894f8d9
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-19 13:14:48 +0000

    update code in pages.views.py

--- news/pages/views.py
+++ news/pages/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# create a template view for the homepage
+class HomePageView(TemplateView):
+    template_name = 'home.html'
