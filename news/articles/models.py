6a7f38755a79bf93f051b65b07fd7f8e811d5c75
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-27 04:00:35 +0000

    Created a new article model

--- news/articles/models.py
+++ news/articles/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=225)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
