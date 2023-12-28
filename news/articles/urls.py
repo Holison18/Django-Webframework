7ff0776a218bd8446a40330f6a9c09505da63e09
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-28 04:27:37 +0000

    urls.py

--- news/articles/urls.py
+++ news/articles/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path 
-from .views import ArticleListView,ArticleDetailView,ArticleUpdateView,ArticleDeleteView
+from django.urls import path
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
+
 # url patterns
 urlpatterns = [
-    path("<int:pk>/",ArticleDetailView.as_view(),"article_detail"),
-    path("<int:pk>/edit/",ArticleUpdateView.as_view(),"article_edit"),
-    path("<int:pk>/delete/",ArticleDeleteView.as_view(),"article_delete"),
-    path("",ArticleListView.as_view(),name="article_list")
+    path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("", ArticleListView.as_view(), name="article_list"),
 ]
