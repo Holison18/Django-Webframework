4bcfe885d50a9fb50f89ec755fd7824370486fb7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2023-12-28 04:23:57 +0000

    url paths for edit,delete and detail views

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,UpdateView,DeleteView
 from .models import Article
 
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+
 
