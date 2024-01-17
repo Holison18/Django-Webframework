90b3bd98d980cd9647a481fed0faab2e4b904e0d
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-01-17 06:47:08 +0000

    Configuring database for production

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "default": env.dj_db_url("DATABASE_URL")
     }
 }
 
