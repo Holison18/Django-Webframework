681da858b79d68d5dd490706c43f6a2d02e81574
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-01-18 05:24:59 +0000

    Created a directory for static files

--- news/django_project/settings.py
+++ news/django_project/settings.py
@@ -74,9 +74,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "default": env.dj_db_url("DATABASE_URL")
-    }
+    "default": env.dj_db_url("DATABASE_URL"),
 }
 
 
