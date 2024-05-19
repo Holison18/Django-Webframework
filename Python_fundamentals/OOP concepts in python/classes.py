31cc98b32fcba8403f42495e1d49dd77df247c24
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-19 14:48:37 +0000

    classes.py

--- Python_fundamentals/OOP concepts in python/classes.py
+++ Python_fundamentals/OOP concepts in python/classes.py
@@ -1,3 +1,17 @@
 # creating a dog class
 class Dog(object):
     """ A simple attempt to model a dog"""
+
+    def __init__(self,name,age) -> None:
+        self.name = name
+        self.age = age
+
+    # create a sit function for the dog
+    def sit(self):
+        """Simulate a dog sitting in response to a command"""
+        print(self.name.title() + " is now sitting")
+
+    def rollover(self):
+        """simulate dog rolling over in response to a command"""
+        print(self.name.title() + " rolled over")
+
