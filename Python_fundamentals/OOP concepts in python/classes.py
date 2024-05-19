fed1b37bfa324e3556d733b3a4b97a10bd5dd684
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-19 15:24:35 +0000

    classes.py

--- Python_fundamentals/OOP concepts in python/classes.py
+++ Python_fundamentals/OOP concepts in python/classes.py
@@ -15,6 +15,21 @@ class Dog(object):
         """simulate dog rolling over in response to a command"""
         print(self.name.title() + " rolled over")
 
+class Restaurant(object):
+    "This a restaurant class"
+
+    def __init__(self, name:str, cuisine_type:str):
+        self.name = name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        """Describes the restaurant"""
+        print(f"The name of the restaurant is {self.name}")
+        print(f"Cuisine Type: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.name} restaurant is opened now!")
+
 
 # creat an instance of the dog class
 hope = Dog("Hope", 24)
@@ -24,3 +39,18 @@ hope.rollover()
 # Print the name and ageof the dog hope
 print(f"My dog's name is {hope.name.title()}.")
 print(f"My dog is {hope.age} years old.")
+
+
+print()
+print()
+
+# create an instance of the restaurant class
+Edziban = Restaurant("Edziban Delight","French Cuisine")
+Edziban.describe_restaurant()
+Edziban.open_restaurant()
+
+# Print the attributes of the restaurant class
+print("Name of Restaurant: {}".format(Edziban.name.title()))
+print("Cuisine: {}".format(Edziban.cuisine_type))
+
+
